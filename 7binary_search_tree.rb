eae92c2d2247e5092c6eaa028568c7b3d1e37227
RainDropsz <raindropsz100@gmail.com>
2020-05-09 12:36:44 -0400

    finish iterative build tree

--- 7binary_search_tree.rb
+++ 7binary_search_tree.rb
@@ -17,6 +17,7 @@ Be sure to always remove duplicate values or check for an
 existing value before inserting.
 =end
 
+require 'benchmark'
 
 class Node
   attr_accessor :value, :left, :right
@@ -33,10 +34,52 @@ class Tree
   attr_reader :root
 
   def initialize(array)
-    @root =  build_tree(array.sort.uniq)
+    Benchmark.bm do |x|
+      x.report("iteration: ")  { build_tree(array) }
+      x.report("recursion: ")  { build_tree_recursion(array) }
+    end
   end
 
-  def build_tree(array)
+
+  def build_tree(array) #uses iteration,= breadth first
+    array = array.sort.uniq
+    root = Node.new( array [array.length / 2] )
+    @root = root
+    bfs_queue = []
+    k = 0
+
+
+    2.upto(Math.log2(array.length) + 1) do |i|
+      1.step(2**i, 4) do |j|
+#         p "i = #{i} ; j = #{j} ; root = #{root.value}"
+         left_index = (j) * array.length / (2**i)
+         right_index = (j + 2) * array.length / (2**i) 
+
+         if array[left_index]            
+           root.left = Node.new(array [ left_index ] )
+            array[ left_index ] = nil
+#           p "Created root.left = #{root.left.value}"
+           bfs_queue << root.left 
+         end
+
+         if array[right_index]
+           root.right = Node.new(array [ right_index] )
+            array[ right_index ] = nil
+#           p "Created root.right = #{root.right.value} "
+           bfs_queue << root.right
+         end
+        root = bfs_queue[k]
+#        bfs_queue.each {|x| puts x.value;}
+        k += 1
+      end
+    end
+      
+    @root
+
+  end
+
+  
+  def build_tree_recursion(array)
     root = Node.new( array [ array.length / 2 ] )
 
     return root if array.length <= 1
@@ -46,11 +89,20 @@ class Tree
 
     return root 
   end
+
+  def insert(value)
+    
+  end
+
+  def del(value)
+
+  end
+
   def print_tree
-    puts "#{ @root.value}  \n"
-    puts "#{@root.left.value}  #{@root.right.value}  \n"
-    puts "#{@root.left.left.value}  #{@root.left.right.value}  #{@root.right.left.value}  #{@root.right.right.value} \n"
-    puts "#{@root.left.left.left.value}  #{@root.left.left.right.value}  #{@root.left.right.left.value}  #{@root.left.right.right.value}  #{@root.right.left.left.value}  #{@root.right.left.right.value}     #{@root.right.right.left.value}  #{@root.right.right.right.value}" 
+#    p "#{@root.value}  \n"
+#    p "#{@root.left.value}  #{@root.right.value}  \n"
+#    p "#{@root.left.left.value}  #{@root.left.right.value}  #{@root.right.left.value}  #{@root.right.right.value} \n"
+#    p "#{@root.left.left.left.value}  {@root.left.left.right.value}  #{@root.left.right.left.value}  {@root.left.right.right.value}  {@root.right.left.left.value}  #{@root.right.left.right.value}     {@root.right.right.left.value}  #{@root.right.right.right.value}" 
   end
 end
 
@@ -58,15 +110,16 @@ end
 
 a = [ 1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 b = [10,7,14,20,1,5,8,3, 9].sort.uniq   # [1, 5, 7, 8, 10, 14, 20]
-tree = Tree.new(a)
-p a.sort.uniq
+c = [0,1,2,3,4,5,6,7,8,9,10]
+d = (1..100000).map {rand}
+tree = Tree.new(d)
 tree.print_tree
-
 
 
 =begin
 -----------------------------------------------
 My notes for this headache!!! 
+build_tree_recursion
 [1, 3, 4, 5, 7, 8, 9, 23, 67, 324, 6345]
 root = Node 8
 root.left = build[1,3,4,5,7]  
@@ -88,5 +141,16 @@ root.left = build_tree([1,5,7])
   root = Node 5
   root.left = build_tree([1])       root.left = Node 1
     root = Node 1                  
+-------------------------
+build_tree iteration
+
+n = length of array
+height of tree = log2(n) + 1
+Used breadth first search queue to traverse nodes
+                              1/2n
+                  1/4n                      3/4n
+           1/8n          3/8n         5/8n         7/8n
+      1/16n  3/16n   5/16n 7/16n   9/16n 11/16n 13/16n  15/16n
+
 =end
 
